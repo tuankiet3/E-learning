d6add74534e03f44c291f92e10599c585b4981b6
votrunghieu89 <votrunghieu4@dtu.edu.vn>
2025-06-25 15:26:56 +0700

    add Services/GenerateID.cs

--- Services/GenerateID.cs
+++ Services/GenerateID.cs
@@ -1,6 +1,51 @@
-﻿namespace E_learning.Services
+﻿using System.Security.Cryptography;
+namespace E_learning.Services
 {
     public class GenerateID
     {
+        // automatically generate unique IDs for Course
+        public string generateCourseID() {
+            byte[] bytes = new byte[4];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(bytes);
+            }
+            int number = Math.Abs(BitConverter.ToInt32(bytes, 0)) % 1000000;
+            string numberString = number.ToString("D6");
+            return $"CID{numberString}";
+        }
+        // automatically generate unique IDs for Lesson
+        public string generateLessonID() {
+            byte[] bytes = new byte[4];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(bytes);
+            }
+            int number = Math.Abs(BitConverter.ToInt32(bytes, 0)) % 1000000;
+            string numberString = number.ToString("D6");
+            return $"LID{numberString}";
+        }
+        // automatically generate unique IDs for Quiz
+        public string generateQuizID() {
+            byte[] bytes = new byte[4];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(bytes);
+            }
+            int number = Math.Abs(BitConverter.ToInt32(bytes, 0)) % 1000000;
+            string numberString = number.ToString("D6");
+            return $"QID{numberString}";
+        }
+        // automatically generate unique IDs for Choice
+        public string generateChoiceID() {
+            byte[] bytes = new byte[4];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(bytes);
+            }
+            int number = Math.Abs(BitConverter.ToInt32(bytes, 0)) % 1000000;
+            string numberString = number.ToString("D6");
+            return $"CHID{numberString}";
+        }
     }
 }
